e741b8f193d83367efa3bbc574977af68987e180
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-11-15 20:04:45 -0600

    Chart shows on the page

--- covid-app/src/components/Chart/Chart.jsx
+++ covid-app/src/components/Chart/Chart.jsx
@@ -3,25 +3,38 @@ import { Line, Bar } from 'react-chartjs-2';
 import { fetchDailyData } from '../../api';
 //import styles from './Chart.module.css';
 function Chart() {
-	const [dailyData, setDailyData] = useState({});
+	const [dailyData, setDailyData] = useState([]);
 	useEffect(() => {
 		const fetchAPI = async () => {
-			setDailyData(await fetchDailyData);
+			setDailyData(await fetchDailyData());
 		};
 		console.log('This is the daily data', dailyData);
 		fetchAPI();
 	}, []);
 
-	const LineChart = dailyData[0] ? (
+	const LineChart = dailyData.length ? (
 		<Line
-			data={{ labels: dailyData(({ date }) => date), datasets: [{}, {}] }}
+			data={{
+				labels: dailyData.map(({ date }) => date),
+				datasets: [
+					{
+						data: dailyData.map(({ confirmed }) => confirmed),
+						label: 'Infected',
+						borderColor: 'gray',
+						fill: true,
+					},
+					{
+						data: dailyData.map(({ deaths }) => deaths),
+						label: 'Infected',
+						borderColor: 'red',
+						backgroundColor: 'rgba(255,0,0,0.4)',
+						fill: true,
+					},
+				],
+			}}
 		/>
 	) : null;
-	return (
-		<div>
-			<p>Chart</p>
-		</div>
-	);
+	return <div className={StyleSheet.container}>{LineChart}</div>;
 }
 
 export default Chart;
